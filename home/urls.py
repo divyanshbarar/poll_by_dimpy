4dfdc4214861f58f356b9ac29d7152d98f9c24fb
Divyansh Barar <68277579+divyanshbarar@users.noreply.github.com>
2020-11-20 22:04:53 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from home import views
 
+
+admin.site.site_header="Poll Page Admin"
+admin.site.site_title=" Welcome Divyansh"
+admin.site.index_title="This is Poll Page Backend"
+
 urlpatterns = [
     path('', views.home,name='home'),
     path('cretae/', views.create,name='create'),
